9f979e03585e4bbcd1960e838cbe4e8e183014b3
louiscklaw <louiscklaw@gmail.com>
2021-06-08 14:10:35 +0800

    update and test,

--- mp4_to_gif.sh
+++ mp4_to_gif.sh
@@ -3,11 +3,15 @@
 set -ex
 
 
-ffmpeg -y -ss 30 -t 30 -i viutv_test_bg.mp4 -vf "fps=10,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 viutv_test_bg.gif &
+# ffmpeg -y -ss 30 -t 30 -i viutv_test_bg.mp4 -vf "fps=10,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 viutv_test_bg.gif &
 
-ffmpeg -y -ss 30 -t 30 -i youtube.mp4 -vf "fps=10,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 youtube.gif &
+# ffmpeg -y -ss 30 -t 30 -i youtube.mp4 -vf "fps=10,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 youtube.gif &
 
-ffmpeg -y -ss 30 -t 30 -i kite.mp4 -vf "fps=10,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 kite.gif &
+# ffmpeg -y -ss 30 -t 30 -i kite.mp4 -vf "fps=10,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 kite.gif &
+
+# ffmpeg -y -ss 30 -t 20 -i hong_kong_nightview.mp4 -vf "fps=10,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 hong_kong_nightview.gif &
+
+ffmpeg -y -ss 30 -t 15 -i japan_university.mp4 -vf "fps=10,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 japan_university.gif &
 
 wait
 
