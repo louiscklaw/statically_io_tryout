539f2a6d632973bffe7e74c8f67b729d2ba67cfd
louiscklaw <louiscklaw@gmail.com>
2021-06-08 13:42:38 +0800

    update and test,

--- NOTES.md
+++ NOTES.md
@@ -2,6 +2,6 @@ https://cdn.statically.io/gh/louisckalw/statically_io_tryout/manage/viutv_test_b
 
 https://cdn.statically.io/gh/louiscklaw/statically_io_tryout/6f0c0aed/helloworld.mp4
 
-https://cdn.statically.io/gh/louiscklaw/statically_io_tryout/b1622747/helloworld.html
+https://cdn.statically.io/gh/louiscklaw/statically_io_tryout/master/helloworld.html
 
 https://cdn.statically.io/gh/louiscklaw/statically_io_tryout/4ef3f959/viutv_test_bg.gif
--- mp4_to_gif.sh
+++ mp4_to_gif.sh
@@ -3,6 +3,13 @@
 set -ex
 
 
-ffmpeg -ss 30 -t 3 -i viutv_test_bg.mp4 -vf "fps=10,scale=960:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 viutv_test_bg.gif
+ffmpeg -y -ss 30 -t 30 -i viutv_test_bg.mp4 -vf "fps=10,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 viutv_test_bg.gif &
 
-ffmpeg -ss 30 -t 3 -i youtube.mp4 -vf "fps=10,scale=960:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 youtube.gif
+ffmpeg -y -ss 30 -t 30 -i youtube.mp4 -vf "fps=10,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 youtube.gif &
+
+ffmpeg -y -ss 30 -t 30 -i kite.mp4 -vf "fps=10,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 kite.gif &
+
+wait
+
+
+echo 'done'
